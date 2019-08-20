257d825b76b7b1ad472f4a68516047ac76cae924
wikt <jbsmith4491@gmail.com>
2019-08-20 11:20:23 +1000

    Fixed issue with large backup requests failing due to parser size limit

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ import v1Router from './routes/APIv1';
 var app = express();
 
 app.use(logger('dev'));
-app.use(express.json());
+app.use(express.json({limit: '10mb'}));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
@@ -20,7 +20,6 @@ app.use((err, req, res, next) => {
   if (!err) {
       return next();
   }
-
   res.status(500);
   res.send('500: Internal server error');
 });
