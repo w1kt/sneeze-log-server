4c3f1731e55b9ca13d33c597c10dac6ab1a1ab2e
wikt <jbsmith4491@gmail.com>
2019-09-18 15:27:05 +1000

    Added new endpoint stub to recover user password. Added configuration and test email send to new endpoint

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -1,6 +1,7 @@
 import UserService from "../services/User";
 import Helpers from "../utils/Helpers";
 import { NoRecordFoundError } from "../utils/CustomErrors";
+import nodemailer from "nodemailer";
 
 const User = {
   /**
@@ -72,6 +73,33 @@ const User = {
       }
       return res.status(statusCode).send({ message: error.message });
     }
+  },
+  async forgotPassword(req, res) {
+    const auth = {
+     type: 'OAuth2',
+     user: 'loggableapp@gmail.com',
+     clientId: '729794276972-06rd2lkqlnvt2m26a5387v91omt0n6h2.apps.googleusercontent.com',
+     clientSecret: 'Mu0gT7p6eF1U44KCHZB7oMWf',
+     refreshToken: '1//04u5SdD2oAL3UCgYIARAAGAQSNwF-L9Ir-FMcy1POzJr0ns8_OeAa7BDCk9GGnchNH-MbcOIBZGQwBk2Km8DNwux1oFxY2g2ZIHg'
+   }
+   let transporter = nodemailer.createTransport({
+     service: 'gmail',
+     auth
+   });
+   const mailOpts = { 
+    from: 'Loggable <noreply@loggable-app.com>',
+    to: 'jbsmith4491@gmail.com',
+    subject: 'test',
+    text: 'this is a test',
+    html: '<p>this is a test</p>'
+   }
+  transporter.sendMail(mailOpts, (err, res) => {
+    if (err) {
+      return console.log('err');
+    } else {
+      console.log(JSON.stringify(res))
+    }
+  })
   }
 };
 
