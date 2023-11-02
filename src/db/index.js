171b698fbf8fef3f31d4760702f92b9977c9bacc
unknown <jbsmith4491@gmail.com>
2023-11-02 13:54:12 +1100

    replace db connection string

--- src/db/index.js
+++ src/db/index.js
@@ -2,7 +2,11 @@ import { Pool } from 'pg';
 
 const pool = new Pool(
   {
-    connectionString: process.env.DATABASE_URL,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
   }
 ) // See .env file for connection settings
 
